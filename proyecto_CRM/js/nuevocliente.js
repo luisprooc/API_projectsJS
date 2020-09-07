bf9300d2fb20e61465c42f23ca40629fa1bc4dec
Luis Miguel Rosario <luisprooc@gmail.com>
2020-09-07 08:07:25 -0500

    Se creo la funcionalidad de agregar los clientes a IndexedDB

--- proyecto_CRM/js/nuevocliente.js
+++ proyecto_CRM/js/nuevocliente.js
@@ -6,7 +6,7 @@
     document.addEventListener("DOMContentLoaded", () =>{
         conectarDB();
 
-        formulario.addEventListener("click",validarCliente);
+        formulario.addEventListener("submit",validarCliente);
     });
 
     function conectarDB(){
@@ -35,8 +35,22 @@
         const empresa = document.getElementById("empresa").value;
 
         if(nombre === "" || email === "" || telefono === "" || empresa === ""){
-            imprimirAlerta("Todos los campos son obligatorios","error")
+            imprimirAlerta("Todos los campos son obligatorios","error");
+            return; 
         }
+
+        // crear un ojeto con la informacion
+
+        const cliente = {
+            // No se coloca llave y valor porque son lo mismo
+            nombre,
+            email,
+            telefono,
+            empresa,
+            id : Date.now()
+        }
+
+        crearCliente(cliente);
     }
 
     function imprimirAlerta(mensaje,tipo){
@@ -67,5 +81,30 @@
                 divMensaje.remove();
             },3000);
             }
+    }
+
+    function crearCliente(cliente){
+        // Crear transaccion
+        const transaction = DB.transaction(["crm"],"readwrite");
+
+        // Definir el ObjectStore
+        const objectStore = transaction.objectStore("crm");
+
+        objectStore.add(cliente);
+
+        // Si ocurre un error mostrar alerta
+        transaction.onerror = () =>{
+            imprimirAlerta("Hubo un error en la transaccion","error");
+        }
+
+        // Si se completa mostrar alerta
+        transaction.oncomplete = () =>{
+            imprimirAlerta("Cliente agregado","success");
+
+            // Luego de 3 segundos mandar a la localizacion de la pagina principal
+            setTimeout(() => {
+                window.location.href = "index.html";
+            },3000);
+        }
     }
 })();
