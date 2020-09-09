58f57f054c2283029050c79ccc0bd45ad06bfc74
Luis Miguel Rosario <luisprooc@gmail.com>
2020-09-09 14:52:37 -0800

    Se agrego la funcion de eliminar los registros del DOM y de IndexedDB

--- proyecto_CRM/js/app.js
+++ proyecto_CRM/js/app.js
@@ -1,7 +1,7 @@
 // Los IIFE sirven para que no puedas acceder a los metodos definidos aqui desde otro archivo
 (()=>{
     let DB;
-
+    const listadoClientes = document.querySelector("#listado-clientes");
 
     document.addEventListener("DOMContentLoaded",()=>{
         crearDB();
@@ -9,6 +9,8 @@
         if(window.indexedDB.open("crm",1)){
             obtenerClientes();
         }
+
+        listadoClientes.addEventListener("click",eliminarRegistro);
     });
 
     // crear base de datos
@@ -67,7 +69,7 @@
                 if(cursor){
                     const {nombre, empresa, email, telefono, id} = cursor.value;
 
-                    const listadoClientes = document.querySelector("#listado-clientes");
+
                     listadoClientes.innerHTML += `
                     <tr>
                         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
@@ -82,7 +84,7 @@
                         </td>
                         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
                             <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
-                            <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+                            <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
                         </td>
                     </tr>
                     `;
@@ -91,6 +93,32 @@
 
                 else{
                     console.log("No hay mas registror");
+                }
+            }
+        }
+    }
+
+    function eliminarRegistro(e){
+        // Delegation al boton eliminar
+
+        if(e.target.classList.contains("eliminar")){
+            const idEliminar = Number(e.target.dataset.cliente);
+
+            const confirmar = confirm("¿Deseas eliminar este cliente?");
+
+            if(confirmar){
+                const transaction = DB.transaction(["crm"],"readwrite");
+
+                const objectStore = transaction.objectStore("crm");
+
+                objectStore.delete(idEliminar);
+
+                transaction.oncomplete = () =>{
+                    e.target.parentElement.parentElement.remove();
+                }
+
+                transaction.onerror  = () =>{
+                    console.log("Ha ocurrido un error");
                 }
             }
         }
