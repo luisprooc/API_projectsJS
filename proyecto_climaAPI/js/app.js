26ac7e856470a69c5e321eaa9ca471464c6bea8c
Luis Miguel Rosario <luisprooc@gmail.com>
2020-09-16 04:45:26 -0800

    Se creo la funcion de validar el formulario

--- proyecto_climaAPI/js/app.js
+++ proyecto_climaAPI/js/app.js
@@ -1,9 +1,4 @@
-// Variables
-
-const container = document.querySelector(".container");
-const resultado = document.querySelector("#resultado");
-const formulario = document.querySelector("#formulario");
-
+import {formulario,ciudad, pais, container} from "./variables.js";
 
 // Eventos
 window.addEventListener("load", () =>{
@@ -14,6 +9,38 @@ window.addEventListener("load", () =>{
 function buscarClima(e){
     e.preventDefault();
 
-    console.log("Obtener clima");
     // Validar
+    if(ciudad.value === "" || pais.value === ""){
+        // Hubo un error
+        mostrarError("Ambos campos son obligatorios");
+        return;
+    }
+
+    else{
+        console.log(pais,ciudad)
+    }
+}
+
+function mostrarError(mensaje){
+
+    const alerta = document.querySelector(".bg-red-100");
+    if(!alerta){
+        // crear alerta
+        const alerta = document.createElement("div");
+
+        alerta.classList.add("bg-red-100","border-red-400","text-red-700","px-4","py-3",
+        "rounded","max-w-md","mx-auto","mt-6","text-center");
+
+        alerta.innerHTML = `
+            <strong class = "font-bold"> ERROR </strong>
+            <span class = "block"> ${mensaje} </span>
+        `;
+        container.appendChild(alerta);
+
+        // Eliminar alerta
+        
+        setTimeout(() =>{
+            alerta.remove()
+        },3000);
+    }
 }
---
+++ proyecto_climaAPI/js/variables.js
@@ -0,0 +1,5 @@
+export const container = document.querySelector(".container");
+export const resultado = document.querySelector("#resultado");
+export const formulario = document.querySelector("#formulario");
+export const ciudad = document.getElementById("ciudad");
+export const pais = document.getElementById("pais");
