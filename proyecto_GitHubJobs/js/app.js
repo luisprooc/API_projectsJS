6fc925f2f0c791a864045f78e616484506cc61c3
Luis Miguel Rosario <luisprooc@gmail.com>
2020-09-23 16:44:36 -0400

    Se creo la funcion de traer y mostrar los datos de la API

--- proyecto_GitHubJobs/js/app.js
+++ proyecto_GitHubJobs/js/app.js
@@ -13,6 +13,8 @@ function validarBusqueda(e){
         mostrarMensaje("La informacion que proporcionaste no es valida");
         return;
     }
+
+    consultarAPI(busqueda);
 }
 
 function mostrarMensaje(mensaje){
@@ -27,5 +29,36 @@ function mostrarMensaje(mensaje){
         setTimeout(() =>{
             alerta.remove();
         },3000);
+    }
+}
+
+function consultarAPI(busqueda){
+    const gitHubUrl = `https://jobs.github.com/positions.json?search=${busqueda}`;
+    const url = `https://api.allorigins.win/get?url=${encodeURIComponent(gitHubUrl)}`;
+    axios.get(url)
+        .then(respuesta => mostrarVacantes(JSON.parse(respuesta.data.contents)))
+}
+
+function mostrarVacantes(vacantes){
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
+
+    if(vacantes.length > 0){
+        resultado.classList.add("grid");
+
+        vacantes.forEach(vacante =>{
+            const {company,title,type,url} = vacante;
+
+            resultado.innerHTML += `
+                
+            <div class="shadow bg-white p-6 rounded">
+                <h2 class="text-2xl font-light mb-4">${title}</h2>
+                <p class="font-bold uppercase">Compañia:  <span class="font-light normal-case">${company} </span></p>
+                <p class="font-bold uppercase">Tipo de Contrato:   <span class="font-light normal-case">${type} </span></p>
+                <a class="bg-teal-500 max-w-lg mx-auto mt-3 rounded p-2 block uppercase font-xl font-bold text-white text-center" href="${url}">Ver Vacante</a>
+            </div>
+            `;
+        });
     }
 }
